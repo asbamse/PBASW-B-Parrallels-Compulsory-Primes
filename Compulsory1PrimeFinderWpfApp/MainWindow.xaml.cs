635c8a052afc489dab12e17dfc366f8a4056c9ce
Asbjørn Mansa Jensen <asbj0378@easv365.dk>
2020-09-18 12:02:32 +0200

    WPF Program for prime finder

---
+++ Compulsory1PrimeFinderWpfApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Compulsory1PrimeFinderLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Compulsory1PrimeFinderWpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ParallelPrimeFinder PrimeFinder { get; set; } = new ParallelPrimeFinder();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            int start = int.Parse(TextBox_StartNumber.Text);
+            int end = int.Parse(TextBox_EndNumber.Text);
+            List<int> primes = await Task.Run(() => PrimeFinder.GetPrimesBetween(start, end));
+            PrimeList.ItemsSource = primes;
+        }
+    }
+}
